8d6ca35c5688e9e8659c50530c6adddb93a518d4
Jenny <jennyberry1330@gmail.com>
2026-02-25 23:59:03 +0530

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,6 @@ import 'package:social_app/theme/dark_mode.dart';
 import 'package:social_app/theme/light_mode.dart';
 import 'package:social_app/firebase_options.dart';
 import 'package:social_app/auth/login_or_register.dart';
-import 'package:social_app/components/my_drawer.dart';
 import 'package:social_app/Pages/loading_page.dart';
 
 void main() async {
